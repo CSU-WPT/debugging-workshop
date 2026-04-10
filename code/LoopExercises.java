a228be98efafaf970a41d0df12b52e1d9628f8d5
RiverStepp <147261489+RiverStepp@users.noreply.github.com>
2026-04-09 22:15:48 -0400

    Create LoopExercises.java

---
+++ code/LoopExercises.java
@@ -0,0 +1,9 @@
+public class LoopExercises {
+    public static int countUpTo(int n) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            count++;
+        }
+        return count;
+    }
+}
