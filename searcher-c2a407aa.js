843e82977673c583de2b02479bc73385479e4f43
RiverStepp <RiverStepp@users.noreply.github.com>
2026-04-10 02:11:43 +0000

    deploy: dcb1d02bcf16723eab1e37952546bbdfa1aa9aeb

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a84dc307.js',
+                path_to_root + 'searchindex-f9d30b69.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
